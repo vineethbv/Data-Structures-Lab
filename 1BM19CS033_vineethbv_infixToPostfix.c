9c2ed1ef50ee81252ac013db964d3a673f2dd7eb
Vineeth B V <71359072+vineethbv@users.noreply.github.com>
2020-10-09 15:03:39 +0530

    Create 1BM19CS033_vineethbv_infixToPostfix.c

---
+++ 1BM19CS033_vineethbv_infixToPostfix.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<ctype.h>
+
+char stack[100];
+int top = -1;
+
+void push(char x)
+{
+    stack[++top] = x;
+}
+
+char pop()
+{
+    if(top == -1)
+        return -1;
+    else
+        return stack[top--];
+}
+
+int priority(char x)
+{
+    if(x == '(')
+        return 0;
+    if(x == '+' || x == '-')
+        return 1;
+    if(x == '*' || x == '/')
+        return 2;
+    return 0;
+}
+
+int main(void)
+{
+    char exp[100];
+    char *e, x, *postFix;
+    int i = 0;
+    printf("Enter the expression : ");
+    scanf("%s",exp);
+    printf("\n");
+    e = exp;
+    
+    while(*e != '\0')
+    {
+        if(isalnum(*e))
+            printf("%c ",*e);
+        else if(*e == '(')
+            push(*e);
+        else if(*e == ')')
+        {
+            while((x = pop()) != '(')
+                printf("%c ", x);
+        }
+        else
+        {
+            while(priority(stack[top]) >= priority(*e))
+                printf("%c ",pop());
+            push(*e);
+        }
+        e++;
+    }
+    
+    while(top != -1)
+    {
+        postFix[i] = pop();
+        i++;
+    }
+
+    for (int j = 0; j <= i; j++)
+      printf("%c", postFix[j]);
+}
